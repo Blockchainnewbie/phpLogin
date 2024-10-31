b3a35124a295acc79e342af3ced60adcb5e52a5e
Sonny Ram <contact@blockchainnewbie.com>
2024-10-31 22:43:31 +0100

    Huge Update

---
+++ php_basic.php
@@ -0,0 +1,15 @@
+<?php
+
+        // definition Variables
+        // $ ist zur Variabllen defenition
+
+    $my_string = "Hello";
+    $my_integer = 34;
+    $my_float = 233.33;
+    $my_bool = true;
+    $my_array = [];
+    $my_char = 'c';
+
+    // type string ist ein Array von Charachter Type!!!
+
+    
